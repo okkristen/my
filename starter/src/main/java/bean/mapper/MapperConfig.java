bef9334aac818c7f146b5041445e51bdcb4201be
ok克里斯汀 <402605042@qq.com>
2021-04-05 13:55:35 +0800

    增加。验证码

---
+++ starter/src/main/java/bean/mapper/MapperConfig.java
@@ -0,0 +1,12 @@
+package bean.mapper;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 持久层bean注入
+ * @author okkristen
+ * @date 2021年04月04日 19:39
+ */
+@Configuration // 配置注解
+public class MapperConfig {
+}
---
+++ starter/src/main/java/bean/services/ServiceConfig.java
@@ -0,0 +1,12 @@
+package bean.services;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 业务层bean注入
+ * @author okkristen
+ * @date 2021年04月04日 19:35
+ */
+@Configuration // 配置注解
+public class ServiceConfig {
+}
